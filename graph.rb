21f640baa923ff2ae11b6bd745fa0551b46d6341
Anastasia <nastia@nastia-Vostro-1520.(none)>
2013-04-15 20:09:38 -0600

    Added comments. Code complete

--- graph.rb
+++ graph.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
+# Author: Anastasia Shpurik
+# Professor: Dinesh Mehta
+# Course: CSCI406
+# Project3: Graph Modeling 
 
 require_relative 'edge'
 
@@ -11,14 +15,15 @@ lines.each do |line|
   input.push(line) if (line.length > 0)
 end
 
-#store first line of file as 'n', number of points
+#store first line of file as number of villages
 num_villages = input[0].split(' ')[0]
+#store second line of file as number of routes
 num_lines = input[0].split(' ')[1]
 layout = []
 
 i=1
 j=0
-#store each edge into an array of coordinates (both directions)
+#store each edge into an array of edges (both directions)
 while i<input.length
   village1 = input[i].split(' ')[0]
   village2 = input[i].split(' ')[1]
@@ -31,6 +36,7 @@ while i<input.length
   i+=1
 end 
 
+#create adjacency lists for each edge
 def make_adjlists(layout)
   layout.each do |edge|
     start = edge.village1
@@ -47,8 +53,10 @@ def make_adjlists(layout)
   end
 end
 
+#dfs algorithm
 def dfs(layout)
   make_adjlists(layout)
+  #for each vertex, if it hasn't been visited, visit it
   layout.each do |edge|
     if edge.visited == 0
       dfs_visit(edge)
@@ -57,18 +65,24 @@ def dfs(layout)
 end
 
 def dfs_visit(edge)
+  #mark the vertex you are currently on
   edge.set_visited(1)
+  #for each adjacent vertex, if it hasn't been visited, visit it
   edge.adj_list.each do |adj|
     if adj.visited == 0
+      #assign the parent vertex of each vertex
       adj.set_parent(edge)
       dfs_visit(adj)
     end
   end
+  #mark the edge as visited
   edge.set_visited(2)
 end
 
+#get all of the parent vertices and construct a path
 def storepath(layout, path)
   n=0
+  #the path starts at the end; 'j'
   p = layout[138].parent
   while p != nil
     str = "layout[138]"+".parent"*n
@@ -84,16 +98,20 @@ def storepath(layout, path)
   path.push('A')
 end
 
+#print out the path, from beginning to end
 def printpath(path)
+  puts "Path: From Startsburg (A) to Endenville (j)"
   n = path.size
   while n >= 0
-    puts path[n]
+    print("#{path[n]} ")
     n-=1
   end
+  puts "\n"
+  puts "Path length: #{path.size-2}"
 end
 
+#initialize empty path, do dfs on it
 path = []
 dfs(layout)
 storepath(layout, path)
-puts path.size
 printpath(path)
